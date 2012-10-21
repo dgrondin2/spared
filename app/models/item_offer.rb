defd0792b066bea8b2bf425eddd02aa03c2487a0
dgrondin2 <dgrondin2@gmail.com>
2012-10-20 17:23:10 -0700

    added paperclip (img upload)

--- app/models/item_offer.rb
+++ app/models/item_offer.rb
@@ -1,8 +1,9 @@
 class ItemOffer < ActiveRecord::Base
 	attr_accessible :animal_type, :category, :city, :condition, :description, :estimated_value, 
-					:expiration_date, :image, :name, :state, :zip
+					        :expiration_date, :image, :name, :state, :zip
 
 	validates :name, :presence => true
+  has_attached_file :image, :styles => { :thumb => "50x50>" }
 	
 	belongs_to :donor
 	belongs_to :organization
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121006002144) do
+ActiveRecord::Schema.define(:version => 20121020234628) do
 
   create_table "donations", :force => true do |t|
     t.date     "date_donated"
@@ -71,8 +71,12 @@ ActiveRecord::Schema.define(:version => 20121006002144) do
     t.date     "expiration_date"
     t.text     "description"
     t.binary   "image"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "organizations", :force => true do |t|
