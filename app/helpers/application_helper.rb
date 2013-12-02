dfd0f47bd73d899fcc8683c9722e939cf3c34aa9
Dominic Grondin <dgrondin@onekingslane.com>
2013-12-01 20:37:22 -0800

    fixed upload photo (just need to activate AWS for it to work), and added a function to help prepare for pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,15 @@ module ApplicationHelper
     guess = current_page?(org_login_path) ? 'org' : nil
     guess ||= cookies[:is_member] || 'donor'
   end
+
+  def how_many_to_show(what_to_show, how_many, page)
+    count = what_to_show.count
+    return "0" if count == 0
+    first_num = ((page-1) * how_many) + 1
+    return "0" if first_num > count
+    last_num = first_num + (how_many - 1)
+    last_num = count if last_num > count
+    return "#{first_num}" if first_num == last_num
+    "#{first_num}-#{last_num}"
+  end
 end
